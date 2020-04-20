7f26489db0b5460b7dd3a3220b8edef28457b723
bakarov <amirbakarov@gmail.com>
2020-04-20 15:26:57 +0300

    change project structure

--- beam/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "beam.h"
-
-int main() {
-    Beam* mB = new Beam();
-    std::cout << mB->beamSearch("what a b c twenty two for four and twenty with three hundred fifty five") << std::endl;
-    return 0;
-}
