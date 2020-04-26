c1c98085b219858333db178fea383ae3647d02da
bakarov <amirbakarov@gmail.com>
2020-04-26 11:46:47 +0300

    add fixture test cases

--- beam/src/test/beam_test.cpp
+++ beam/src/test/beam_test.cpp
@@ -7,17 +7,55 @@ namespace bunt = boost::unit_test;
 
 using namespace std;
 
-struct Fixture {
-    Fixture() {}
+struct Fixture
+{
+    static Fixture*& instance() {
+        static Fixture* inst = 0;
+        return inst;
+    }
 
-    ~Fixture() {}
+    Fixture() {
+        instance() = this;
+        Beam beam = Beam();
+    }
+
+    ~Fixture() {
+    }
+
+    Beam beam;
 };
 
-BOOST_FIXTURE_TEST_CASE(beam_test, Fixture) {
-    std::string testLine = "my number is twenty three four five";
-    Beam* beam = Beam::getInstance();
-    std::string true_result = "my number is 2345";
-    std::string pred_result = beam->beamSearch(testLine);
+BOOST_AUTO_TEST_SUITE(BeamTest)
 
-    BOOST_TEST(true_result == pred_result);
-}
+    BOOST_GLOBAL_FIXTURE(Fixture);
+
+    BOOST_AUTO_TEST_CASE(Test1) {
+        const std::string
+                input = "the number is one two three four five six seven eight nine",
+                true_output = "the number is 123456789";
+        const std::string output = Fixture::instance()->beam.beamSearch(input);
+        std::cout << output << std::endl;
+        BOOST_REQUIRE_EQUAL(output == true_output, true);
+    }
+
+    BOOST_AUTO_TEST_CASE(Test2) {
+        const std::string
+                input = "one hundred and two hundred and three thousand",
+                true_output = "100 and 200 and 3000";
+        const std::string output = Fixture::instance()->beam.beamSearch(input);
+        std::cout << output << std::endl;
+        BOOST_REQUIRE_EQUAL(output == true_output, true);
+    }
+
+    BOOST_AUTO_TEST_CASE(Test3) {
+        const std::string
+                input = "twenty two and thirty three and forty four and fifty five",
+                true_output = "22 and 33 and 44 and 55";
+        const std::string output = Fixture::instance()->beam.beamSearch(input);
+        std::cout << output << std::endl;
+        BOOST_REQUIRE_EQUAL(output == true_output, true);
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
